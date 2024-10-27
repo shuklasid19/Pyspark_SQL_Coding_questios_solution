c1d7c9996f996cd7ed1da8223686e6811f887244
Sidharth Shukla <44350763+shuklasid19@users.noreply.github.com>
2024-10-27 13:04:24 +0530

    Update window_unbounded_preceision.sql

--- window_unbounded_preceision.sql
+++ window_unbounded_preceision.sql
@@ -1,1 +1,5 @@
-select player_name,gender,score_points, sum(score_points) OVER (Partition by gender order by day Rows between unbounded preceding and current row) as cumulative_score from players_scores order by gender , day;
+select player_name,gender,score_points, sum(score_points)
+  OVER (Partition by gender order by day Rows between unbounded preceding and current row)
+  as cumulative_score 
+  from players_scores 
+  order by gender , day;
